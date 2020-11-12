ec9495d28656d65195497ab59206807a82bd5f87
Pedot T <thomas.pedot@gmail.com>
2020-11-12 12:06:23 +0100

    Ajout contours iris

--- mednum/config/__init__.py
+++ mednum/config/__init__.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from tqdm import tqdm
 
 external_data = Path('./data/external/')
 processed_data = Path('./data/processed/')
@@ -10,3 +9,7 @@ interim_data = Path('./data/interim/')
 url_france_geojson_zip = 'https://github.com/gregoiredavid/france-geojson/archive/master.zip'
 geojson_path = external_data / "france-geojson"
 geojson_zip_file_path = geojson_path.with_suffix(".zip")
+
+# Data gouv
+url_data_gouv_geojson_zip = "https://www.data.gouv.fr/fr/datasets/r/3665b916-1a0b-4cbe-9bde-eabfcab29872"
+data_gouv_geojson_file_path = external_data / "france-geojson"/ "contours-iris.geojson"
