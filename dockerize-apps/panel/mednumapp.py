1b81243f328b441b791e6f301aabeeb97c1f1f79
Pedot T <thomas.pedot@gmail.com>
2020-12-14 22:35:49 +0100

    Mise à jour du style

--- dockerize-apps/panel/mednumapp.py
+++ dockerize-apps/panel/mednumapp.py
@@ -5,14 +5,10 @@ from jinja2 import Environment, FileSystemLoader
 
 import mednum
 
-css_mednum = [
-    str(Path(__file__).parent / "statics" / "css" / "mednum.css"),
-    str(Path(__file__).parent / "statics" / "css" / "landing-page.min.css"),
-]
-
+css_mednum = list((Path(".") / "statics").rglob("*.css"))
 css = [
     "https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css",
-    "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css",
+    "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap-grid.min.css",
 ] + css_mednum
 js = {
     "$": "https://code.jquery.com/jquery-3.4.1.slim.min.js",
@@ -26,17 +22,15 @@ env = Environment(loader=FileSystemLoader("./templates"))
 jinja_template = env.get_template("mednum.html")
 
 
-# tmpl = pn.Template(template)
 tmpl = pn.Template(jinja_template)
-tmpl.add_variable("app_title", "<h1>Custom Template App</h1>")
+tmpl.add_variable("app_title", "Portail de la fragilité numérique un outils de MED NUM")
 
 mednumapp = mednum.MedNumApp(name="Sélection")
 
 # Top indicator
 tmpl.add_panel("sidebar", mednumapp.lat_widgets)
-tmpl.add_panel("top", pn.Row(mednumapp.top_panel, sizing_mode="stretch_width")),
+tmpl.add_panel("top", mednumapp.top_panel),
 tmpl.add_panel(
-    "main", mednumapp.tabs_view,
-)
+    "main", mednumapp.tabs_view)
 
 tmpl.servable()
